c06481b10451f3a02a62858f6c46d8ad76a891f8
Iber <iber.fernandez@mojix.com>
2016-11-08 19:45:32 -0400

    GoogleMaps

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,33 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers',['ionic'])
 
-.controller('DashCtrl', function($scope) {})
+.controller('MapController', function($scope, $ionicLoading) {
+ console.log('entro');
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    });
+ 
+})
+
+
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
